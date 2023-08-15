0c23658536bf441c3fb73ecde9f129f5a28cb323
AJackTi <dtrong97vn@gmail.com>
2023-08-15 23:15:54 +0700

    feat: add compression package

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const compression = require("compression");
 const app = express();
 
+// init middlewares
 // app.use(morgan("combined"));
 // app.use(morgan("tiny"));
 // app.use(morgan("short"));
 // app.use(morgan("common"));
 app.use(morgan("dev"));
 app.use(helmet());
-
-// init middlewares
+app.use(compression());
 
 // init db
 
