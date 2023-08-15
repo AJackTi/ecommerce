dfc5db80d198e8a21eaaae85fd4cb168ba512c41
AJackTi <dtrong97vn@gmail.com>
2023-08-15 23:06:52 +0700

    feat: add morgan package

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+app.use(morgan("dev"));
+// app.use(morgan("combined"));
+// app.use(morgan("tiny"));
+// app.use(morgan("short"));
+// app.use(morgan("common"));
 
 // init middlewares
 
 // init db
 
 // init routes
+app.get("", (req, res, next) => {
+  return res.status(200).json({
+    message: "Welcome to course!",
+  });
+});
 
 // handling error
 
