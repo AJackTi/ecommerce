7722b7a8b5f4075fe9383051fc70fd10ff3cf74b
AJackTi <dtrong97vn@gmail.com>
2023-08-15 23:12:05 +0700

    feat: add helmet package

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet");
 const app = express();
 
-app.use(morgan("dev"));
 // app.use(morgan("combined"));
 // app.use(morgan("tiny"));
 // app.use(morgan("short"));
 // app.use(morgan("common"));
+app.use(morgan("dev"));
+app.use(helmet());
 
 // init middlewares
 
